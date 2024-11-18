49c71cc11d8bec5f5f77fab3f89ad6cdb6915c82
Apoorv3826 <90827294+Apoorv3826@users.noreply.github.com>
2024-11-18 20:33:54 +0530

    Update DashboardPage.jsx
    updated

--- client/src/routes/dashboardPage/DashboardPage.jsx
+++ client/src/routes/dashboardPage/DashboardPage.jsx
@@ -1,7 +1,7 @@
 import { useState } from 'react';
 import { useMutation, useQueryClient } from '@tanstack/react-query';
 import { useNavigate } from 'react-router-dom';
-import styles from './DashboardPage.module.css';
+import styles from './dashboardPage.module.css';
 
 const DashboardPage = () => {
   const queryClient = useQueryClient();
