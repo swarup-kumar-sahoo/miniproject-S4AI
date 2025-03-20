0b4565b848e655962468007ebef539e55533242d
Swarup Kumar Sahoo <105979920+swarup-kumar-sahoo@users.noreply.github.com>
2025-03-20 11:36:10 +0530

    Update gemini.js

--- client/src/lib/gemini.js
+++ client/src/lib/gemini.js
@@ -1,6 +1,6 @@
 import { GoogleGenerativeAI } from "@google/generative-ai";
 
-const genAI = new GoogleGenerativeAI("AIzaSyCNm475ezrucGV1c169H0IIgjX1ftwSgqQ");
+const genAI = new GoogleGenerativeAI("YOUR_API_KEY");
 const model = genAI.getGenerativeModel({ model: "gemini-1.5-flash" });
 
 export default model;
